11df5d7ee4fdde682af14cacc7dd214facc81787
Simon Baines <tinylittleviolence@protonmail.com>
2023-12-04 20:52:59 +0000

    2.2

--- src/2.js
+++ src/2.js
@@ -13,11 +13,12 @@ readGamesFromFile('../files/2.txt')
         return createGameArray(games);
     })
     .then((games) => {
-        //console.log(games)
         return getGameScore(games, bagConfig);
     })
-    .then((score) => {
-        return console.log(`The answer to challenge 2.1 is: ${score}`);
+    .then((scores) => {
+        console.log(`The answer to challenge 2.1 is: ${scores.valid}`);
+        console.log(`The answer to challenge 2.2 is: ${scores.power}`);
+        return;
     })
 
 
@@ -55,6 +56,9 @@ async function parseGameData (game) {
 
     let gameData = {};
     let gameSets = [];
+    let redLowest = 0;
+    let greenLowest = 0;
+    let blueLowest = 0;
 
     let gameId = parseInt(game.split(":")[0].match(/\d+/)[0]);
     //console.log(gameId);
@@ -75,8 +79,37 @@ async function parseGameData (game) {
 
             let gamePull = {
                 colour: pullData[2],
-                number: pullData[1]
+                number: parseInt(pullData[1])
             };
+
+            console.log(gamePull)
+
+            switch (gamePull.colour) {
+                case 'red':
+                    console.log(`Colour is ${gamePull.colour}, number is ${gamePull.number}, lowest for this is currently ${redLowest}`)
+                    console.log(gamePull.number > redLowest);
+                    if (gamePull.number > redLowest) {
+                        redLowest = gamePull.number;
+                        console.log(`Setting redLowest to ${gamePull.number}`)
+                    }
+                    break;
+
+                case 'green':
+                    console.log(`Colour is ${gamePull.colour}, number is ${gamePull.number}, lowest for this is currently ${greenLowest}`)
+                    if (gamePull.number > greenLowest) {
+                        greenLowest = gamePull.number;
+                        console.log(`Setting greenLowest to ${gamePull.number}`)
+                    }
+                    break;
+                        
+                case 'blue':
+                    console.log(`Colour is ${gamePull.colour}, number is ${gamePull.number}, lowest for this is currently ${blueLowest}`)
+                    if (gamePull.number > blueLowest) {
+                        blueLowest = gamePull.number;
+                        console.log(`Setting blueLowest to ${gamePull.number}`)
+                    }
+                    break;
+            }
 
             gamePulls.push(gamePull);
 
@@ -90,14 +123,18 @@ async function parseGameData (game) {
 
         //console.log(gameSet);
         gameSets.push(gameSet);
-        
+
     }
 
     gameData.gameId = gameId;
     gameData.numberOfSets = sets.length;
     gameData.sets = gameSets;
+    gameData.redLowest = redLowest;
+    gameData.blueLowest = blueLowest;
+    gameData.greenLowest = greenLowest;
+    gameData.power = redLowest * blueLowest * greenLowest;
 
-    //console.log(gameData);
+    console.log(gameData);
    
     return gameData;
 
@@ -105,7 +142,9 @@ async function parseGameData (game) {
 
 async function getGameScore(games, config) {
 
-    let score = 0;
+    let validScore = 0;
+    let powerScore = 0;
+    let scores = {};
 
     for (game of games) {
 
@@ -114,11 +153,22 @@ async function getGameScore(games, config) {
         isValid = await isValidGame(game, config);
       
         if (isValid) {
-          score += game.gameId;
+            validScore += game.gameId;
         }
     }
 
-    return score;
+    for (game of games) {
+
+        powerScore += game.power;
+
+    }
+
+    scores =  {
+        valid: validScore,
+        power: powerScore
+    };
+
+    return scores;
 }   
 
 async function isValidGame(game, config) {
@@ -129,10 +179,10 @@ async function isValidGame(game, config) {
 
             for (limit of config.colourLimits) {
 
-                console.log(`Set ${set.setId} in game ${game.gameId}: checking to see if ${pull.colour}: ${pull.number} violated the ${limit.colour}: max ${limit.number} rule.`)
+                //console.log(`Set ${set.setId} in game ${game.gameId}: checking to see if ${pull.colour}: ${pull.number} violated the ${limit.colour}: max ${limit.number} rule.`)
 
                 if (pull.colour === limit.colour && pull.number > limit.number) {
-                  console.log(`Set ${set.setId} in game ${game.gameId} violated a colour limit rule. It is not a valid game.`);
+                  //console.log(`Set ${set.setId} in game ${game.gameId} violated a colour limit rule. It is not a valid game.`);
                   return false;
                 }
 
